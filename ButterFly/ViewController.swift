8ed799130979dab0e1f2183b41ada51810961225
oscarwang05 <oscarwang1205@gmail.com>
2022-06-13 23:06:37 +1000

    Initial Setup

--- ButterFly/ViewController.swift
+++ ButterFly/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    // Set up navigation bar
+    func navigationBarSetup() {
+        
+    }
+    
+    
+    
+    // MARK: TableView Delegate & DataSource
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        return cell
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
 
 }
 
